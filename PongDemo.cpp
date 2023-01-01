63a96790c19b83a2312a9eb0432a5815b83a0905
BlackBigG <tw0900172332@gmail.com>
2023-01-02 07:31:02 +0800

    fun:ballmove+fixbug

--- PongDemo.cpp
+++ PongDemo.cpp
@@ -11,6 +11,7 @@ typedef struct
 	int y;
 	int widthsize;
 	int heightsize;
+	int speed;
 }obj;
 
 typedef struct
@@ -26,12 +27,13 @@ typedef struct
 void showrec(obj ex);
 obj playermovement(obj);
 void showball(ball);
-
+ball ballmove(ball, obj, obj);
+ball ballacceleration(ball, obj);
 int main()
 {
 	obj player = { -300,0,10,40 };
 	obj enemy = { 300,0,10,40 };
-	ball ball = { 0,0,10,5,5 };
+	ball ball = { 0,0,10,10,0 };
 
 	initgraph(width, height, NULL);
 	BeginBatchDraw();
@@ -40,6 +42,7 @@ int main()
 	while (true)
 	{
 		player = playermovement(player);
+		ball = ballmove(ball, player, enemy);
 		showrec(player);
 		showrec(enemy);
 		showball(ball);
@@ -56,14 +59,16 @@ int main()
 
 obj playermovement(obj ex)
 {
+	ex.speed = 0;
 	if (GetAsyncKeyState(VK_UP))
 	{
-		ex.y -= 10;
+		ex.speed = -10;
 	}
 	if (GetAsyncKeyState(VK_DOWN))
 	{
-		ex.y += 10;
+		ex.speed = 10;
 	}
+	ex.y += ex.speed;
 	return ex;
 }
 void showrec(obj ex)
@@ -74,3 +79,40 @@ void showball(ball ex)
 {
 	solidcircle(ex.x, ex.y, ex.rad);
 }
+ball ballmove(ball ex, obj player, obj enemy)
+{
+
+	ex.x += ex.speedx;
+	ex.y += ex.speedy;
+	if (abs(ex.x) == width / 2 - ex.rad ||
+		(ex.x - ex.rad == player.x + player.widthsize) && (player.y - player.heightsize <= ex.y && ex.y <= player.y + player.heightsize))
+	{
+		ex = ballacceleration(ex, player);
+		ex.speedx *= -1;
+	}
+	if ((ex.x + ex.rad == enemy.x - enemy.widthsize) && (enemy.y - enemy.heightsize <= ex.y && ex.y <= enemy.y + enemy.heightsize))
+	{
+		ex = ballacceleration(ex, enemy);
+		ex.speedx *= -1;
+	}
+	if (ex.y <= -300 + ex.rad || ex.y >= 300 - ex.rad ||
+		(ex.y + ex.rad == player.y - player.heightsize || ex.y - ex.rad == player.y + player.heightsize) && player.x - player.widthsize <= ex.x && ex.x <= player.x + player.widthsize ||
+		(ex.y + ex.rad == enemy.y - enemy.heightsize || ex.y - ex.rad == enemy.y + enemy.heightsize) && enemy.x - enemy.widthsize <= ex.x && ex.x <= enemy.x + enemy.widthsize
+		)
+	{
+		ex.speedy *= -1;
+	}
+	return ex;
+}
+	ball ballacceleration(ball ball, obj obj)
+	{
+		if (obj.speed > 0)
+		{
+			ball.speedy += 3;
+		}
+		if (obj.speed < 0)
+		{
+			ball.speedy -= 3;
+		}
+		return ball;
+	}
