8d219a90e817f24c0866c8ea2df8fae237186ab7
BlackBigG <tw0900172332@gmail.com>
2023-01-02 01:13:49 +0800

    Struct:obj_Define

--- PongDemo.cpp
+++ PongDemo.cpp
@@ -1,9 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <easyx.h>
+
+#define width 800
+#define height 600
+
+typedef struct
+{
+	int x;
+	int y;
+	int widthsize;
+	int heightsize;
+}obj;
+
+
+
+
+obj playermovement(obj);
+
 
 int main()
 {
 
 	system("pause");
 	return 0;
+}
+
+
+obj playermovement(obj ex)
+{
+	if (GetAsyncKeyState(VK_UP))
+	{
+		ex.y -= 10;
+	}
+	if (GetAsyncKeyState(VK_DOWN))
+	{
+		ex.y += 10;
+	}
+	if (GetAsyncKeyState(VK_LEFT))
+	{
+		ex.x -= 10;
+	}
+	if (GetAsyncKeyState(VK_RIGHT))
+	{
+		ex.x += 10;
+	}
+	return ex;
 }
